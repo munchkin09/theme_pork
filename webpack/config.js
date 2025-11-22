2c07827aabed2b73161bfac9a765c4a1303398a5
Carlos Ledesma <carlos.developer1983@gmail.com>
2025-11-22 12:42:00 +0100

    feat: refactor entry point to use entry.js, update Webpack configuration for development and production

--- webpack/config.js
+++ webpack/config.js
@@ -6,7 +6,7 @@ const webpack = require("webpack");
 module.exports = {
     mode: "development",
     devtool: "eval-source-map",
-    entry: "./src/game.js",
+    entry: "./src/entry.js",
     output: {
         path: path.resolve(process.cwd(), 'dist'),
         filename: "bundle.min.js"
